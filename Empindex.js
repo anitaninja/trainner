07dc4d3f43f448384a03e1d8578cc8795b370f1a
anitaninja <singh.anita.ninja@gmail.com>
2017-01-30 18:22:25 +0530

    Work With Image Upload and Store.

--- Empindex.js
+++ Empindex.js
@@ -1,13 +1,12 @@
-
 var express = require('express');
 var app = express();
 var router = express.Router();
-var multer  = require('multer');
+var multer = require('multer');
 
 var morgan = require('morgan');
 app.use(morgan('dev'));
 var bodyParser = require('body-parser');
-
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
@@ -18,8 +17,24 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/Image'); // connect to our database
 var Img = require('./Empmongo');
 
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/ProductImages');
+    },
+    filename: function (req, file, cb) {
+        console.log("----");
+        var filename = Date.now() + file.originalname;
+        req.body.EmpImage = filename;
+        console.log(req.body.EmpImage);
+        cb(null, filename);
+    }
+})
+
+var upload = multer({storage: storage}).any();
+
+
 router.route('/Emp')
-    .get( function (req, res, next) {
+    .get(function (req, res, next) {
         Img.find().exec(function (err, result) {
             if (err)
                 res.send(err);
@@ -29,37 +44,38 @@ router.route('/Emp')
 
     })
 
-    .post(function (req, res) {
-        var img=new Img();
-        img.EmpName=req.body.EmpName;
-        img.EmpSalary=req.body.EmpSalary;
-        img.EmpDept=req.body.EmpDept;
+    .post(upload,function (req, res) {
+        var img = new Img();
+        img.EmpName = req.body.EmpName;
+        img.EmpImage=req.body.EmpImage;
+        img.EmpSalary = req.body.EmpSalary;
+        img.EmpDept = req.body.EmpDept;
 
         img.save(function (err) {
             if (err)
                 res.send(err);
             res.send("Employ Entry Done !!!! ");
         });
-    })
-router.put('/Emp/:id', function(req, res) {
-        Img.findById(req.params.id, function (err, img) {
+    });
+router.put('/Emp/:id', function (req, res) {
+    Img.findById(req.params.id, function (err, img) {
+        if (err)
+            res.send(err);
+
+        img.EmpName = req.body.EmpName;
+        img.EmpSalary = req.body.EmpSalary;
+        img.EmpDept = req.body.EmpDept;
+        img.save(function (err) {
             if (err)
                 res.send(err);
 
-            img.EmpName = req.body.EmpName;
-            img.EmpSalary = req.body.EmpSalary;
-            img.EmpDept = req.body.EmpDept;
-            img.save(function (err) {
-                if (err)
-                    res.send(err);
-
-                res.json(img);
-            });
-
+            res.json(img);
         });
+
     });
-router.delete('/Emp/:id', function(req, res) {
-    var img=new Img();
+});
+router.delete('/Emp/:id', function (req, res) {
+    var img = new Img();
     Img.remove({_id: req.params.id},
         function (err) {
             if (err)
@@ -68,21 +84,21 @@ router.delete('/Emp/:id', function(req, res) {
             res.json({message: 'Successfully deleted'});
         });
 });
-router.get('/Emp/:id',function (req, res, next) {
-        Img.findById(req.params.id, function (err, user) {
-            if (err)
-                res.send(err);
+router.get('/Emp/:id', function (req, res, next) {
+    Img.findById(req.params.id, function (err, user) {
+        if (err)
+            res.send(err);
 
-            res.json(user);
-        });
+        res.json(user);
+    });
 
-    })
-router.route('/page')
+})
+router.route('/DataEntry')
     .get(function (req, res, next) {
         res.sendfile(__dirname + '/EmpDataEntry.html');
     });
 
 app.use('/', router);
-app.listen(8080);
+app.listen(3000);
 
 
